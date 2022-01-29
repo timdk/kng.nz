e1b9dcdb112351d76da7855c79e0222e92203051
Tim King <tim@kng.nz>
2022-01-29 11:16:56 +0100

    Update global stylesheet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import { AppProps } from 'next/app'
 
-import '@exampledev/new.css/new.css';
-import '@fontsource/inter';
 import '@/styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
