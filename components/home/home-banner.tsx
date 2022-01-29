28d9685d77d2fd7f1bee54f95a156227da77e84c
Tim King <tim@kng.nz>
2022-01-29 18:15:50 +0100

    Update stylelint config, add webpack config for converting class names from kebab-case to camelCase

--- components/home/home-banner.tsx
+++ components/home/home-banner.tsx
@@ -2,7 +2,7 @@ import styles from './home-banner.module.scss';
 
 const HomeBanner = () => (
   <div className={styles.banner}>
-    <h1 className={styles.banner_text}>kng.nz</h1>
+    <h1 className={styles.bannerText}>kng.nz</h1>
   </div>
 );
 
