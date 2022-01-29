d4c09257c7ee4548540cde5da98a48c8f8719a33
Tim King <tim@kng.nz>
2022-01-29 11:18:25 +0100

    Add index with banner

---
+++ components/home/home-banner.tsx
@@ -0,0 +1,9 @@
+import styles from './home-banner.module.scss';
+
+const HomeBanner = () => (
+  <div className={styles.banner}>
+    <h1 className={styles.banner_text}>kng.nz</h1>
+  </div>
+);
+
+export default HomeBanner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,13 @@
-import {GetStaticProps, NextPage} from "next";
-import Link from 'next/link'
+import { NextPage } from 'next';
 
-import { getSortedPostsData } from '@/lib/posts'
+import Layout from '@/layouts/layout';
+import HomeBanner from '@/components/home/home-banner';
 
-import Layout  from '../components/layout'
-import Date from '../components/date'
-
-interface Props {
-  allPostsData: {
-    id: string,
-    date: Date,
-    title: string
-  }[]
-}
-
-const Home: NextPage<Props> = ({ allPostsData }) => {
+const Home: NextPage = () => {
   return (
-    <Layout home>
-      <section>
-        <h2>Blog</h2>
-        <ul>
-          {allPostsData.map(({ id, date, title }) => (
-            <li key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+    <Layout>
+      <HomeBanner/>
     </Layout>
-  )
-}
+  );
+};
 export default Home;
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
