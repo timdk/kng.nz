5579ad066bdf05cb8529b750488133d5cd269ace
Tim King <tim@kng.nz>
2022-02-06 17:13:35 +0100

    Add deploy configuration for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: !debug ? '/kng.nz/' : '',
   // https://github.com/vercel/next.js/discussions/11267#discussioncomment-1758
   webpack: (config) => {
-    const rules = config.module.rules.find((rule) => typeof rule.oneOf === 'object').oneOf.filter((rule) => Array.isArray(rule.use));
+    const rules = config.module.rules.find((rule) => typeof rule.oneOf === 'object').oneOf.filter((rule) => Array.isArray(rule.use))
 
     rules.forEach((rule) => {
       rule.use.forEach((moduleLoader) => {
@@ -12,12 +16,12 @@ module.exports = {
             moduleLoader.options.modules = {
               ...moduleLoader.options.modules,
               exportLocalsConvention: 'camelCaseOnly', // https://github.com/webpack-contrib/css-loader#exportlocalsconvention
-            };
+            }
           }
         }
-      });
-    });
+      })
+    })
 
-    return config;
+    return config
   },
 }
