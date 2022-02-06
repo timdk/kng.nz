b01901fd28e9afda95ed4796731b51f9c0937673
Tim King <tim@kng.nz>
2022-02-06 17:13:14 +0100

    Fix lint issues

--- src/components/home/home-banner.tsx
+++ src/components/home/home-banner.tsx
@@ -1,9 +1,9 @@
-import styles from './home-banner.module.scss';
+import styles from './home-banner.module.scss'
 
 const HomeBanner = () => (
   <div className={styles.banner}>
     <h1 className={styles.bannerText}>kng.nz</h1>
   </div>
-);
+)
 
-export default HomeBanner;
+export default HomeBanner
